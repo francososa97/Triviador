44e65ba7f2a0961e9650c0d3639d6d19b4a420d0
francososa97 <franco.gonzalez.sosa@gmail.com>
2021-04-25 20:03:03 +0200

    feat: init trivias

--- src/Paginas/Trivia.js
+++ src/Paginas/Trivia.js
@@ -1,41 +1,76 @@
 import React,{useContext,useState} from 'react';
-import Generos from '../Helpers/Generos';
 import Container from '@material-ui/core/Container';
-import { NavLink } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import {TriviadorContext} from '../Context/TriviadorContext';
+import { NavLink } from 'react-router-dom';
 
 const Trivia = () => {
-    const { generoElegido,trivias }  = useContext(TriviadorContext);
-    const [triviaPorGenero,SetTriviaPorGenero] = useState({});
-    const propiedadesTrivia = Object.keys(trivias);
-    propiedadesTrivia.forEach(trivia => {
-        let esGeneroSeleccionado = trivia.includes(generoElegido);
-        if(esGeneroSeleccionado){
-            console.log(propiedadesTrivia);
-        }
-    });
+
+    const { generoElegido,triviaPorGenero,usuario,SetPuntaje }  = useContext(TriviadorContext);
+    const [preguntaActual,SetPreguntaActual] = useState(1);
+    const [finalizarTrivia,SetFinalizarTrivia] = useState(false);
+    const triviaActual = triviaPorGenero[preguntaActual];
+
+    const EvaluarRespuesta= (opcionCorrecta) =>{
+        if(opcionCorrecta)
+            SetPuntaje((state) => state + 1);
+        SetPreguntaActual((state) => state + 1);
+        if(preguntaActual === 10)
+            SetFinalizarTrivia(true)
+    }
+
     return(
         <Container maxWidth="sm">
         <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
+            <p>{usuario} eligio el genero de...</p>
             <Typography className="titulo" variant="h1" gutterBottom>
-            Trivia
+                {generoElegido.replace(/_|#|-|@|<>/g, " ")}
             </Typography>
-            <Grid container spacing={3}>
-                <p>{generoElegido}</p>
-            </Grid>
+            <Paper>
+                <h3>{triviaActual.pregunta}</h3>
+                <div>
+                    <ul>
+                        {triviaActual.opciones.map(opcion=>{
+                            return(
+                                <li onClick={() => EvaluarRespuesta(opcion.esVerdadera)}>{opcion.opcion}</li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </Paper>
+            {finalizarTrivia
+            ?   <NavLink to='/puntaje' className="nav-link" activeClassName="active"> <p className="white-text">Finalizar</p></NavLink>
+            : null }
         </Typography>
-        <NavLink 
-                to='/puntaje'
-                className="nav-link"
-                activeClassName="active"
-            >
-                <p className="white-text">Finalizar</p>
-        </NavLink>
+        <span className="quiz_screen-counter">{preguntaActual}/{10}</span>
         </Container>
     )
 }
 export default Trivia;
+
+
+/*
+                    <Pregunta/>
+
+                {triviaPorGenero.map(trivia => {
+                        console.log(trivia);
+                        return(
+                            <>
+                                <p>{trivia.pregunta}</p>
+                                <ol>
+                                    {trivia.opciones.map(opcion => {
+                                        return(
+                                            <>
+                                                <li>
+                                                {opcion.opcion}
+                                                </li>
+                                            </>
+                                        )
+                                    })}
+                                </ol>
+                            </>
+                        )
+                    })}
+*/
