be1582cd1f7fc7e0189106bdc168338541c15631
francososa97 <franco.gonzalez.sosa@gmail.com>
2021-05-02 21:48:49 +0200

    feati : init front

--- src/Paginas/Home.js
+++ src/Paginas/Home.js
@@ -7,6 +7,15 @@ import {TriviadorContext} from '../Context/TriviadorContext';
 import Button from '@material-ui/core/Button';
 
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+
 const Home = (props) =>{
 
     const {generosAleatorios,trivias} = props;
@@ -26,8 +35,115 @@ const Home = (props) =>{
         SetPodesContinuar(puedeContinuar);
     }
 
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+          marginTop: theme.spacing(8),
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        },
+        avatar: {
+          margin: theme.spacing(1),
+          backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+          width: '100%', // Fix IE 11 issue.
+          marginTop: theme.spacing(3),
+        },
+        submit: {
+          margin: theme.spacing(3, 0, 2),
+        },
+      }));
+
+      const classes = useStyles();
+
     return(
-        <Container maxWidth="sm">
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Triviador
+        </Typography>
+          <form className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+              <TextField
+                color = "secondary"
+                id="name"
+                variant="outlined"
+                required
+                fullWidth
+                name="Tu nombre"
+                label="Tu nombre"
+                type="Tu nombre"
+                id="name"
+                value={usuario} 
+                onChange={(e) =>SetearNombre(e.target.value)}
+              />
+            </Grid>
+            </Grid>
+            {  podesContinuar ?
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                >
+                    Comenzar
+                </Button>
+                    
+                    :
+                <Button
+                type="submit"
+                fullWidth
+                variant="disabled"
+                color="primary"
+                className={classes.submit}
+                >
+                    Comenzar
+                </Button>
+
+            }
+          </form>
+        </div>
+      </Container>
+  
+    )
+}
+export default Home;
+
+
+/*
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField id="name" label="Tu nombre" value={usuario} onChange={(e) =>SetearNombre(e.target.value)}/>
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+
+
+
+
+    <Container maxWidth="sm">
             <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
                 <Typography className="titulo" variant="h1" gutterBottom>
                     Home
@@ -59,6 +175,5 @@ const Home = (props) =>{
             </Typography>
 
       </Container>
-    )
-}
-export default Home;
+
+*/
